b00aa1e4e7d758183c7054de6397b9d1a0b4ccce
Joako <joako12409@gmail.com>
2023-11-21 13:57:16 -0300

    ADD NavBar

---
+++ Nautica Rios Web/src/components/navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Nabvar.module.css'; 
+
+const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <Link to="/" className={styles.link}>
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <Link to="/barcos" className={styles.link} disabled={!isLoggedIn}>
+            Agregar Barcos
+          </Link>
+        </li>
+      </ul>
+      <div>
+        {isLoggedIn ? (
+          <button onClick={handleLogout} className={styles.button}>
+            Cerrar Sesión
+          </button>
+        ) : (
+          <button onClick={handleLogin} className={styles.button}>
+            Iniciar Sesión
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+
+
+
+
