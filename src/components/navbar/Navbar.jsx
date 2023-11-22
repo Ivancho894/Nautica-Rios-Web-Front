81369f7962564b48f57cce40d6f57beae3c4ab68
Joako <joako12409@gmail.com>
2023-11-22 09:41:01 -0300

    Mejorando Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './nabvar.module.css'; 
+import Logo from '../../assets/logo.png'
 
 const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -15,15 +16,28 @@ const Navbar = () => {
 
   return (
     <nav className={styles.nav}>
+      <div className={styles.logoContainer}>
+        <img src={Logo} alt="Logo" className={styles.logo} />
+      </div>
       <ul>
         <li>
           <Link to="/" className={styles.link}>
-            Inicio
+            Home
           </Link>
         </li>
         <li>
           <Link to="/barcos" className={styles.link} disabled={!isLoggedIn}>
             Agregar Barcos
+          </Link>
+        </li>
+        <li>
+          <Link to=""className={styles.link}>
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link to=""className={styles.link}>
+            Quienes Somos
           </Link>
         </li>
       </ul>
