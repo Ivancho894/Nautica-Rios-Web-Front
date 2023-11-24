9705ab4cb7695b7941f265c66c5f19d389f68d2d
Joako <joako12409@gmail.com>
2023-11-24 17:52:51 -0300

    Estilos quienes somos identicos al figma!

--- src/components/Paginas/quienesSomos.jsx
+++ src/components/Paginas/quienesSomos.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './quienesSomos.module.css';
-import Logo from '../../assets/logo.png';
+import Logo from '../../assets/Logonegro.png';
 
 export default function QuienesSomos(){
     return (
@@ -9,10 +9,8 @@ export default function QuienesSomos(){
                 <h1 className={styles.title}>Sobre Nosotros</h1>
                 <h2 className={styles.subtitle}>Somos una empresa apasionada por brindar soluciones náuticas. Desde el 2001 nos dedicamos a la comercialización de embarcaciones nuevas y usadas, representando a los astilleros Génesis y Segue.
                 </h2>
-                <h3 className={styles.values}>Contamos con experiencia internacional en el rubro. Nuestro conocimiento comercial y técnico sobre la construcción de barcos, nos posiciona como un socio ideal para concretar tu operación de la forma más segura posible.
-
-                    Nuestros valores son la seriedad, el compromiso y la confidencialidad.
-                </h3>
+                <h3 className={styles.values}>Contamos con experiencia internacional en el rubro. Nuestro conocimiento comercial y técnico sobre la construcción de barcos, nos posiciona como un socio ideal para concretar tu operación de la forma más segura posible.</h3>
+                <h4 className={styles.subtitle}> Nuestros valores son la seriedad, el compromiso y la confidencialidad.</h4>
             </div>
             <img src={Logo} alt="Nautica Rios" />
         </div>
