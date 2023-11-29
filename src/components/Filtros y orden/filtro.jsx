57c6a6c3fe44a0431a65143118730867aec3db80
Joako <joako12409@gmail.com>
2023-11-29 11:23:00 -0300

    Emparejado de filtro con borde.ref.#41

--- src/components/Filtros y orden/filtro.jsx
+++ src/components/Filtros y orden/filtro.jsx
@@ -2,25 +2,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import { GET_BARCOS, ADD_FILTER, SET_FILTER, GET_FILTERS } from '../../redux/actions';
 import { useEffect, useState } from 'react';
 
-
-
 export default function Filtros(){
-  // const [filtros,setFiltros] = useState({})
-  const allFilters = useSelector(state => state.allFilters)
-  const filtrosAplicados = useSelector(state => state.filter)
-  const dispatch = useDispatch()
-  useEffect(()=>{
+ const allFilters = useSelector(state => state.allFilters)
+ const filtrosAplicados = useSelector(state => state.filter)
+ const dispatch = useDispatch()
+ useEffect(()=>{
 
     // dispatch(GET_FILTERS())
       // setFiltros()
     },[])
 
-  const handleChange= (event)=>{
+ const handleChange= (event)=>{
     dispatch(ADD_FILTER({name:event.target.name,value:event.target.value}))
     dispatch(SET_FILTER())
 
-  }
-  return (
+ }
+ return (
     <div className="bg-blue-200 text-black-200 p-5 h-32 w-full">
       <div className="flex justify-between items-center">
       {allFilters?.map((filtro,i)=>{
@@ -34,12 +31,12 @@ export default function Filtros(){
               return (
               <div key={i} className="flex-wrap justify-between items-center p-5 h-40 gap-5 w-3/4">
                 <label>{prop}:</label>
-                <select  onChange={(event)=>handleChange(event)} name={prop}>
-                  <option value="-">-</option>
+                <select className="rounded-md" onChange={(event)=>handleChange(event)} name={prop}>
+                 <option value="-">-</option>
                   
-                  {filtro[prop].map((rango,i) =>{     
+                 {filtro[prop].map((rango,i) =>{     
                     return <option className="text-black" value={`{"min":${rango.min},"max":${rango.max}}`} key={i}>{rango.min}-{rango.max}</option>
-                  })}
+                 })}
                 
                 </select>
               </div>)
@@ -49,12 +46,12 @@ export default function Filtros(){
             return (
             <div key={i} className="flex-wrap justify-between items-center p-5 h-40 gap-5 w-full">
               <label>{prop}:</label>
-              <select onChange={(event)=>handleChange(event)} name={prop} >
+              <select className="rounded-lg" onChange={(event)=>handleChange(event)} name={prop} >
                 <option value="-">-</option>
                 
                 {filtro[prop].map((value,i)=>{
-                  return <option className="text-black" value={value} key={i}>{value}</option>
-                  })}
+                 return <option className="text-black" value={value} key={i}>{value}</option>
+                 })}
                 
               </select>
             </div>
@@ -65,5 +62,5 @@ export default function Filtros(){
           </div>
           </div>
  
-  )
+ )
 }
