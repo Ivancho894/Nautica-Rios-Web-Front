d323a9740cea55301171b54e94eaa1f980ffbc61
MauricioRV12 <mauriciorico43@gmail.com>
2023-12-01 10:30:52 -0600

    Estilos

--- src/components/accesorios/DetalleAccesorios.jsx
+++ src/components/accesorios/DetalleAccesorios.jsx
@@ -1,7 +1,74 @@
+// import React, { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import { db } from "../../../firebase-config";
+// import { doc, getDoc } from "firebase/firestore"; 
+
+// export default function DetalleAccesorio() {
+//   const { id } = useParams();
+//   const [accesorio, setAccesorio] = useState(null);
+
+//   useEffect(() => {
+//     const fetchAccesorio = async () => {
+//       const accesorioDoc = doc(db, 'accesorios', id);
+//       const accesorioSnapshot = await getDoc(accesorioDoc);
+      
+//       if (accesorioSnapshot.exists()) {
+//         setAccesorio({
+//           id: accesorioSnapshot.id,
+//           ...accesorioSnapshot.data()
+//         });
+//       } 
+//     };
+
+//     fetchAccesorio();
+//   }, [id]);
+
+//   if (!accesorio) {
+//     return <p>Cargando...</p>;
+//   }
+
+//   return (
+//     <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 bg-blue-200">
+
+//       {accesorio.variaciones && accesorio.variaciones.length > 0 && (
+//         <div>
+//           {accesorio.variaciones.map((variacion, variacionIndex) => (
+//             <div key={variacionIndex}>
+//               {variacion.imagenes && variacion.imagenes.length > 0 && (
+//                 <div>
+//                   {variacion.imagenes.map((imagen, imagenIndex) => (
+//                     <img key={imagenIndex} src={imagen} alt={`Imagen ${imagenIndex + 1}`} style={{ maxWidth: '20%' }}/>
+//                   ))}
+//                 </div>
+//               )}
+//             </div>
+//           ))}
+//         </div>
+//         )}
+
+//       <h3>
+//         {accesorio.nombre} {accesorio.marca}
+//         {accesorio.descripcion}
+//       </h3>
+//       <h4>
+//         {accesorio.material} {accesorio.precio}
+//         {accesorio.color} {accesorio.peso}
+//       </h4>
+
+//     </div>
+//   );
+// }
+
+
+
+
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../../../firebase-config";
-import { doc, getDoc } from "firebase/firestore"; 
+import { doc, getDoc } from "firebase/firestore";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function DetalleAccesorio() {
   const { id } = useParams();
@@ -11,13 +78,13 @@ export default function DetalleAccesorio() {
     const fetchAccesorio = async () => {
       const accesorioDoc = doc(db, 'accesorios', id);
       const accesorioSnapshot = await getDoc(accesorioDoc);
-      
+
       if (accesorioSnapshot.exists()) {
         setAccesorio({
           id: accesorioSnapshot.id,
           ...accesorioSnapshot.data()
         });
-      } 
+      }
     };
 
     fetchAccesorio();
@@ -27,25 +94,39 @@ export default function DetalleAccesorio() {
     return <p>Cargando...</p>;
   }
 
-  return (
-    <div>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
+  return (
+    <div className="mb-5">
       {accesorio.variaciones && accesorio.variaciones.length > 0 && (
         <div>
           {accesorio.variaciones.map((variacion, variacionIndex) => (
             <div key={variacionIndex}>
               {variacion.imagenes && variacion.imagenes.length > 0 && (
-                <div>
+                <Slider {...settings} className="w-80 mx-auto">
                   {variacion.imagenes.map((imagen, imagenIndex) => (
-                    <img key={imagenIndex} src={imagen} alt={`Imagen ${imagenIndex + 1}`} style={{ maxWidth: '20%' }}/>
+                   <div key={imagenIndex} className="w-80 h-85">
+                   <img
+                     src={imagen}
+                     alt={`Imagen ${imagenIndex + 1}`}
+                     className="max-w-full h-auto rounded-3xl"
+                     style={{ maxWidth: '100%', maxHeight: '100%' }}
+                   />
+                 </div>
                   ))}
-                </div>
+                </Slider>
               )}
             </div>
           ))}
         </div>
-        )}
-
+      )}
+      <br/>
       <h3>
         {accesorio.nombre} {accesorio.marca}
         {accesorio.descripcion}
@@ -54,7 +135,6 @@ export default function DetalleAccesorio() {
         {accesorio.material} {accesorio.precio}
         {accesorio.color} {accesorio.peso}
       </h4>
-
     </div>
   );
 }
--- src/components/accesorios/RenderAccesorios.jsx
+++ src/components/accesorios/RenderAccesorios.jsx
@@ -1,38 +1,90 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// const RenderAccesorios = ({ accesorio }) => {
+//  //Renderiza un solo barco
+//  return (
+//     <div className="border-6 border-gray-300 p-5 w-80 h-85 rounded-3xl bg-sky-400 flex flex-col items-center justify-center text-center mb-5">
+//       {/* //* la imagen es solo referencial, aun falta ver como traer las imgaes de la store de firebase */}
+//       {accesorio.variaciones && accesorio.variaciones.length > 0 && (
+//             <div>
+//             {accesorio.variaciones.map((variacion, variacionIndex) => (
+//               <div key={variacionIndex}>
+//                 {variacion.imagenes && variacion.imagenes.length > 0 && (
+//                   <div>
+//                     {variacion.imagenes.map((imagen, imagenIndex) => (
+//                       <img key={imagenIndex} src={imagen} alt={`Imagen ${imagenIndex + 1}`} style={{ maxWidth: '100%' }}/>
+//                       ))}
+//                   </div>
+//                 )}
+//               </div>
+//             ))}
+//           </div>
+//           )}
+//       <h3>
+//         {accesorio.nombre} {accesorio.marca}
+//       </h3>
+//       <h4>
+//         Material de {accesorio.material}
+//       </h4>
+
+//       <Link to={"/detalleaccesorio/" + accesorio.id}>
+//         <button>más</button>
+//       </Link>
+//     </div>
+//  );
+// };
+
+// export default RenderAccesorios;
+
+
+
+
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const RenderAccesorios = ({ accesorio }) => {
- //Renderiza un solo barco
- return (
-    <div className="border-6 border-gray-300 p-5 w-80 h-85 rounded-3xl bg-sky-400 flex flex-col items-center justify-center text-center mb-5">
-      {/* //* la imagen es solo referencial, aun falta ver como traer las imgaes de la store de firebase */}
-      {accesorio.variaciones && accesorio.variaciones.length > 0 && (
-            <div>
-            {accesorio.variaciones.map((variacion, variacionIndex) => (
-              <div key={variacionIndex}>
-                {variacion.imagenes && variacion.imagenes.length > 0 && (
-                  <div>
-                    {variacion.imagenes.map((imagen, imagenIndex) => (
-                      <img key={imagenIndex} src={imagen} alt={`Imagen ${imagenIndex + 1}`} style={{ maxWidth: '100%' }}/>
-                      ))}
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
-          )}
-      <h3>
-        {accesorio.nombre} {accesorio.marca}
-      </h3>
-      <h4>
-        Material de {accesorio.material}
-      </h4>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
-      <Link to={"/detalleaccesorio/" + accesorio.id}>
-        <button>más</button>
-      </Link>
+  return (
+    <div className="mb-5">
+      {accesorio.variaciones && accesorio.variaciones.length > 0 && (
+        <Slider {...settings} className="w-80 mx-auto">
+          {accesorio.variaciones.map((variacion, variacionIndex) => (
+            variacion.imagenes && variacion.imagenes.length > 0 && (
+              variacion.imagenes.map((imagen, imagenIndex) => (
+                <div key={imagenIndex} className="w-80 h-85">
+                  <img
+                    src={imagen}
+                    alt={`Imagen ${imagenIndex + 1}`}
+                    className="max-w-full h-auto rounded-3xl"
+                    style={{ maxWidth: '100%', maxHeight: '100%' }}
+                  />
+                </div>
+              ))
+            )
+          ))}
+        </Slider>
+      )}
+      <br/>
+        <h3>{accesorio.nombre} {accesorio.marca}</h3>
+        <h4>Material de {accesorio.material}</h4>
+        <Link to={"/detalleaccesorio/" + accesorio.id}>
+          <button>más</button>
+        </Link>
+      
     </div>
- );
+  );
 };
 
 export default RenderAccesorios;
+
