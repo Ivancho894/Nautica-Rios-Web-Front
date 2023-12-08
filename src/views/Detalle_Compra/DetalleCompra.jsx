547d668b2db3bdad4a804301f93199be501089e3
caleb <jhoshmc201@gmail.com>
2023-12-08 10:43:06 -0500

    estilando la pagina de pago

--- src/views/Detalle_Compra/DetalleCompra.jsx
+++ src/views/Detalle_Compra/DetalleCompra.jsx
@@ -16,23 +16,38 @@ const DetalleCompra = () => {
     slidesToScroll: 1,
   };
   return (
-    <div>
+    <div className="flex-col my-16">
       <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <div className="flex justify-around">
-        {carr?.map((producto, index) => (
-          <div
-            className="border-2 border-slate-200 p-5 w-80 h-85 rounded-3xlflex flex-col items-center justify-center text-center mb-5 shadow-[0_5px_40px_1px_rgba(0,0,0,2)]"
-            key={index}
-          >
-            <button className="flex" onClick={() => onDeleteProduct(producto)}>
-              x
-            </button>
-            <br />
-            {/* {producto.variaciones && producto.variaciones.length > 0 && (
+
+      <div className="flexl justify-start">
+        <h2>
+          <strong>TU CARRITO</strong>
+        </h2>
+
+        <span>
+          Total # productos <strong>$</strong>
+        </span>
+
+        <p>
+          Los artículos en tu carrito no están reservados. Termina el proceso de
+          compra ahora para hacerte con ellos.
+        </p>
+      </div>
+      <div className="flex justify-around my-6">
+        <div className="flex-col">
+          {carr?.map((producto, index) => (
+            <div
+              className="border-2 border-slate-200 p-5 w-80 h-85 rounded-3xlflex flex-col items-center justify-center text-center mb-5 shadow-[0_5px_40px_1px_rgba(0,0,0,2)]"
+              key={index}
+            >
+              <button
+                className="flex"
+                onClick={() => onDeleteProduct(producto)}
+              >
+                x
+              </button>
+              <br />
+              {/* {producto.variaciones && producto.variaciones.length > 0 && (
               <Slider {...settings} className="w-60 mx-auto">
                 {producto.variaciones.map(
                   (variacion, variacionIndex) =>
@@ -55,64 +70,37 @@ const DetalleCompra = () => {
               </Slider>
             )} */}
 
-            <h3 className="text-2xl font-bold mb-5">{producto.nombre}</h3>
-            <h3>{producto.marca}</h3>
-            <h4>Material de {producto.material}</h4>
-          </div>
-        ))}
-        {/* {carr.variaciones && carr.variaciones.length > 0 && (
-          <Slider {...settings} className="w-60 mx-auto">
-            {carr.variaciones.map(
-              (variacion, variacionIndex) =>
-                variacion.imagenes &&
-                variacion.imagenes.length > 0 &&
-                variacion.imagenes.map((imagen, imagenIndex) => (
-                  <div
-                    key={imagenIndex}
-                    className="w-full h-64 flex items-center justify-center"
-                  >
-                    <img
-                      src={imagen}
-                      alt={`Imagen ${imagenIndex + 1}`}
-                      className="object-contain w-full h-full"
-                      style={{ maxWidth: "100%", maxHeight: "100%" }}
-                    />
-                  </div>
-                ))
-            )}
-          </Slider>
-        )}
-        <br />
-        <h3 className="text-2xl font-bold mb-5">{carr.nombre}</h3>
-        <h3>{carr.marca}</h3>
-        <h4>Material de {carr.material}</h4>
-
-        <div className="flex items-end gap-16">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            onClick={() => agregarAlCarrito()}
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="hover:text-[#3b82f6] cursor-pointer ml-[20px]  w-[40px] h-[40px]"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-            />
-          </svg>
-
-          <Link to={"/detalleaccesorio/" + carr.id}>
-            <button className="ml-4 p-2 bg-[#3b82f6] text-center text-white mt-8 h-[40px] w-[130px]">
-              + info
-            </button>
+              <h3 className="text-2xl font-bold mb-5">{producto.nombre}</h3>
+              <h3>{producto.marca}</h3>
+              <h4>Material de {producto.material}</h4>
+            </div>
+          ))}
+        </div>
+        <div>
+          <Link>
+            <button>Ir a pagar</button>
           </Link>
-        </div> */}
-        <Link>
-          <button>Ir a pagar</button>
-        </Link>
+          <br />
+          <br />
+
+          <div>
+            <h2>
+              <strong>RESUMUEN DEL PEDIDO</strong>
+            </h2>
+            <div className="flex justify-between">
+              <p># productos</p>
+              <p>$</p>
+            </div>
+            <div className="flex justify-between">
+              <p> total</p>
+              <p>$</p>
+            </div>
+            <div>
+              <p>opciones de pago</p>
+              <span> algunas imagenes</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
