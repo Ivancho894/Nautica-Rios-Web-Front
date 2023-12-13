15dfbd7c798c408d62d0c3ea3b859d853ed41d3c
Joako <joako12409@gmail.com>
2023-12-13 16:59:37 -0300

    Sin Barcos Renderizado.

--- src/components/Todos_los_Barcos/SinBarcos.jsx
+++ src/components/Todos_los_Barcos/SinBarcos.jsx
@@ -5,8 +5,10 @@ export default function SinBarcos(){
 
 
     return (
+        <div className="min-h-screen items-center justify-end p-60 w-full">
         <div className="mt-8 ">
             <h1>Sin barcos</h1>
         </div>
+    </div>
     )
 }
