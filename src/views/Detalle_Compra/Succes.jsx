bcd5d01d3233b4e46523aa3c4235202b74e9f573
Ivan <serralta.ivan@gmail.com>
2023-12-13 23:39:43 -0300

    cambios

--- src/views/Detalle_Compra/Succes.jsx
+++ src/views/Detalle_Compra/Succes.jsx
@@ -6,19 +6,23 @@ import { useEffect } from "react"
 
 export default function successPage(){
     const dispatch = useDispatch()
-    const n = window.location.href = "https://nautica-rios-pf-deploy.netlify.app/home"
+    // const n = window.location.href = "https://nautica-rios-pf-deploy.netlify.app/home"
     //Vaciar carrito
     useEffect(()=>{
         dispatch(VACIAR_CARRITO())}
         ,[])
 
-    setTimeout(() => {
-        n
-        }, "1000");
+    // setTimeout(() => {
+    //     console.log('g')
+    //     n
+    //     }, "10000");
 
 
     return(
         <div>
+            <h1>Success</h1>
+            <h1>Success</h1>
+            <h1>Success</h1>
             <h1>Success</h1>
         </div>
     ) 
