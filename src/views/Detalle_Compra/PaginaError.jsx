0b474e79f3b8d4c78676b180d42e607de84e9290
Ivan <serralta.ivan@gmail.com>
2023-12-14 00:15:51 -0300

    Pagina error y succes. Ref: #111

--- src/views/Detalle_Compra/PaginaError.jsx
+++ src/views/Detalle_Compra/PaginaError.jsx
@@ -1,4 +1,18 @@
 export default function () {
-    return <div> error</div>;
+  
+
+
+    return <div class="w-full max-w-md mx-auto mt-20 p-10">
+    <h1 class="text-5xl font-bold text-center mb-6">Mil disculpas!</h1>
+    <p class="text-xl text-center mb-6">
+        Algo salio mal con la compra.
+        <br/>
+        Un encargado de ventas se va a comunicar con usted en breve
+        Si tienes alguna pregunta o necesitas ayuda, no dudes en ponerte en contacto con nosotros.
+    </p>
+    <a href="home#inicio" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center">
+        Regresar al inicio
+    </a>
+</div>;
   }
   
