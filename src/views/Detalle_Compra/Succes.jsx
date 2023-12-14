94cdc3fdeac5b332cc68701f2121a1b5f05a6e04
Ivan <serralta.ivan@gmail.com>
2023-12-13 23:33:01 -0300

    ruta

--- src/views/Detalle_Compra/Succes.jsx
+++ src/views/Detalle_Compra/Succes.jsx
@@ -1,12 +1,22 @@
+import { useDispatch } from "react-redux"
+import { VACIAR_CARRITO } from "../../redux/actions"
+import { useEffect } from "react"
 
 
 
 export default function successPage(){
-
+    const dispatch = useDispatch()
+    const n = window.location.href = "https://nautica-rios-pf-deploy.netlify.app/home"
     //Vaciar carrito
+    useEffect(()=>{
+        dispatch(VACIAR_CARRITO())}
+        ,[])
+
+    setTimeout(() => {
+        n
+        }, "1000");
 
 
-    
     return(
         <div>
             <h1>Success</h1>
